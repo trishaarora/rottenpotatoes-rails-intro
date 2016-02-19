73340805f94edcc247066ef8508d14541aad8f78
Trisha Arora <tarora@wesleyan.edu>
2016-02-19 18:43:18 +0000

    ugh

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    @all_ratings = Movie.all_ratings
+    #@all_ratings = Movie.all_ratings
     
     # if params[:ratings] != nil then
     #   @ratings = params[:ratings].keys
